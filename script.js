8fce7f5bafee2aa94988bc56a0416034de36d16a
pedrim202 <pedro.augusto..dr11@gmail.com>
2021-11-01 21:29:05 -0300

    Adiciona barra de interação

---
+++ script.js
@@ -0,0 +1,17 @@
+let main = document.getElementById('mainScreen')
+let works = document.getElementById('projectsBox')
+let contacts = document.getElementById('contactsBox')
+
+function mainBox()
+{
+    main.scrollIntoView({behavior: "smooth"});
+}
+
+function worksBox()
+{
+    works.scrollIntoView({behavior: "smooth"});
+}
+function contactsBox()
+{
+    contacts.scrollIntoView({behavior: "smooth"});
+}
